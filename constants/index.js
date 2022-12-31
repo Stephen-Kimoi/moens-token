eca0f9f4658e263afaf7eef58b079ed6a54beaf6
Steve Kimoi <moenisteve@gmail.com>
2022-12-31 09:29:09 +0300

    adding contract abi and contract address

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,8 @@
-const MOENSNFT_CONTRACT_ADDRESS = "0xA8E18CB9D8dbA3f225a833110f29eB975763B685"; 
+const { abi } = require('../utils/MoensToken.json'); 
 
-module.exports = { MOENSNFT_CONTRACT_ADDRESS }; 
+const MOENSNFT_CONTRACT_ADDRESS = "0xA8E18CB9D8dbA3f225a833110f29eB975763B685"; 
+const contractAbi = abi;
+const contractAddress = "0xb86CaFE6d8042fF2645FfE725b66E6e3193Ca2B6"; 
+
+
+module.exports = { MOENSNFT_CONTRACT_ADDRESS, contractAbi, contractAddress }; 
