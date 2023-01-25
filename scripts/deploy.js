cc57b9d008a78f83c5713eafcd13eddbf36f21e6
Steve Kimoi <moenisteve@gmail.com>
2023-01-25 17:34:02 +0300

    new contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,6 @@
+// Contract address: 0x644F94c2d99428b89Bc0D161f5Dd8C64c169BC27
+// Contract address:  0x88B46D4C174a7FB14705cdE6C7A21eC4664a6530
+
 const { ethers } = require("hardhat"); 
 require("dotenv").config({ path: ".env" }); 
 const { MOENSNFT_CONTRACT_ADDRESS } = require('../constants'); 
@@ -9,6 +12,12 @@ async function main() {
     await deployedContract.deployed(); 
 
     console.log("Moens Token contract address: ", deployedContract.address); 
+
+    await deployedContract.mint(1, {
+      value: ethers.utils.parseEther("0.001")
+    }); 
+
+    await deployedContract.claim(); 
 }
 
 main()
