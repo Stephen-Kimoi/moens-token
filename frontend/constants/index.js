4bb0b5972e3ef1933dc6e43731d24b335a09bff2
Steve Kimoi <moenisteve@gmail.com>
2023-02-14 14:38:49 +0300

    new contract address and abi

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -2,4 +2,4 @@ import { abi } from '../utils/MoensToken.json';
 
 export const MOENSNFT_CONTRACT_ADDRESS = "0xA8E18CB9D8dbA3f225a833110f29eB975763B685"; 
 export const contractAbi = abi;
-export const contractAddress = "0xb86CaFE6d8042fF2645FfE725b66E6e3193Ca2B6"; 
+export const contractAddress = "0xbf580e6Ee187cF6cF035E7cf4A1C2FFBEC4e192c"; 
