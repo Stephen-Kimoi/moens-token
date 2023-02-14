620c64914a4f9620b15c4b47af93dc595989282b
Steve Kimoi <moenisteve@gmail.com>
2023-02-14 14:40:37 +0300

    calling new contract functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { contractAbi, contractAddress } from "../constants"; 
+import { contractAddress } from "../constants"; 
+import { moensTokenContract } from './contractInstance';
+import { utils } from 'ethers';
+// import { ethers } from 'ethers';
 
 function App() {
   const [count, setCount] = useState(0)
   const [walletConnected, setWalletConnected] = useState(); 
   const [walletAddress, setWalletAddress] = useState(); 
+  const [currentChainId, setCurrentChainId] = useState(""); 
+
 
   const setChainName = async () => {
     try {
@@ -46,7 +51,6 @@ function App() {
       console.log("Account connected is: ", account); 
 
       setChainName(); 
-      getAmounts(); 
 
     } catch (error) {
       console.error(error)
@@ -69,6 +73,43 @@ function App() {
     window.location.reload(); 
   } 
 
+  const getBalances = async () => {
+    console.log('Getting balances...'); 
+    try {
+      const mtkContract = await moensTokenContract(false); 
+      const mtkContract2 = await moensTokenContract(true); 
+      const mintedTokens = await mtkContract.totalSupply(); 
+      const addressBalance = await mtkContract.balanceOf("0x13Ef924EB7408e90278B86b659960AFb00DDae61"); 
+      // const remainingTokens = await mtkContract2.calculateRemainingSupply(); 
+      console.log('Minted tokens are: ', utils.formatEther(mintedTokens)); 
+      console.log('Address balance is: ', utils.formatEther(addressBalance)); 
+      // console.log('Remaining tokens: ', utils.formatEther(remainingTokens)); 
+    } catch(error){
+      console.error(error); 
+    }
+  }
+
+  const mintToken = async () => {
+    console.log("Minting token...")
+    try {
+      const mtkContract  = await moensTokenContract(true); 
+      const tx = await mtkContract.mint(2, {
+        value: utils.parseEther("0.002")
+      }); 
+      console.log("Sending tokens...."); 
+      await tx.wait(); 
+      
+      console.log("Tokens bought succesfully!"); 
+      getBalances(); 
+    } catch (error){
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getBalances(); 
+  })
+
   return (
     <div className="App">
       <h1>Moens Tokens</h1>
@@ -85,8 +126,35 @@ function App() {
             </button>
           )
         }
-    
-      </div>
+
+        {
+          walletConnected && currentChainId != "Goerli Test Network" && (
+            <button onClick={changeChainId}>
+               Switch to goerli
+            </button>
+          )
+        }
+      </div> 
+      
+      {
+        walletConnected && (
+          <div className='mint'>
+            <div>
+              <input 
+                type="number"
+                placeholder="Amount of MTK you want"
+                onChange={ (e) => {
+                  
+                }}
+              />
+              <button onClick={mintToken}>
+                Buy Token
+              </button>
+            </div>
+          </div>
+        )
+      }
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- frontend/src/contractInstance.js
+++ frontend/src/contractInstance.js
@@ -1,19 +1,59 @@
+import { contractAbi, contractAddress } from "../constants"; 
+import Web3Modal, { providers } from "web3modal";
+import { ethers, Contract } from 'ethers';
 
-export const getProviderOrSigner = async (needSigner = false) => {
+const getProviderOrSigner = async (needSigner = false) => {
     try {
+      const web3Modal = new Web3Modal({
+        network: "goerli", 
+        providerOptions: {}, 
+        disableInjectedProvider: false, 
+      }); 
+
       const { ethereum } = window; 
       let signer; 
       let provider; 
 
       if(ethereum){
-        provider = new ethers.providers.Web3Provider(ethereum); 
+        const instance = await web3Modal.connect(); 
+        provider = new ethers.providers.Web3Provider(instance); 
+
         if (needSigner){
           signer = provider.getSigner(); 
           return signer; 
         } 
+
         return provider; 
+      } else {
+        console.log('Kindly install metamask!'); 
       }
     } catch(error) {
       console.error(error); 
     }
+}
+
+export const moensTokenContract = async (signer) => {
+  try {
+    let mtkContract; 
+    const provOrSigner = await getProviderOrSigner(signer); 
+    if (signer) {
+      console.log(signer)
+      mtkContract = new Contract(
+        contractAddress, 
+        contractAbi, 
+        provOrSigner
+      );  
+    } else {
+      mtkContract = new Contract(
+        contractAddress, 
+        contractAbi, 
+        provOrSigner
+      )
+    } 
+  
+    return mtkContract; 
+
+  } catch (error){
+    console.error(error)
   }
+}
